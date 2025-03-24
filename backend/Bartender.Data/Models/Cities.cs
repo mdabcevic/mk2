5616ac41bbe1da0e0eba01a6f8b35b2b0dbe57f0
maja <maja.dabcevic@gmail.com>
2025-03-24 20:17:31 +0100

    missed model update

---
+++ backend/Bartender.Data/Models/Cities.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bartender.Data.Models;
+
+[Table("cities")]
+public class Cities
+{
+    [Key]
+    [Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [Column("name")]
+    public required string Name { get; set; }
+
+    public ICollection<Places> Places { get; set; } = [];
+
+}
