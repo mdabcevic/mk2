ee958f59c8aeb201480cd274c6104a84a88e5898
maja <maja.dabcevic@gmail.com>
2025-03-26 23:32:38 +0100

    add dtos and mappings

---
+++ backend/Bartender.Domain/DTO/PlaceDto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Bartender.Domain.DTO;
+
+public  class PlaceDto
+{
+    public required string BusinessName { get; set; }
+    public required string Address { get; set; }
+    public required string CityName { get; set; }
+    public required string WorkHours { get; set; } 
+    //public TimeOnly OpensAt { get; set; }
+    //public TimeOnly ClosesAt { get; set; }
+
+    public List<MenuItemDto> MenuItems { get; set; } = [];
+}
---
+++ backend/Bartender.Domain/DTO/UpsertPlaceDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Bartender.Domain.DTO;
+
+public class UpsertPlaceDto
+{
+    public required int BusinessId { get; set; }
+    public required int CityId { get; set; }
+    public required string Address { get; set; }
+    public required string OpensAt { get; set; }
+    public required string ClosesAt { get; set; }
+}
