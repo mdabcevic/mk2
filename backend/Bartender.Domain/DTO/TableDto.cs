28433343f9c4f72105d9bc0661b0f0b2b3321edd
maja <maja.dabcevic@gmail.com>
2025-03-29 20:35:54 +0100

    WIP table dtos

---
+++ backend/Bartender.Domain/DTO/TableDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Bartender.Domain.DTO;
+
+public class TableDto
+{
+    public int PlaceId { get; set; }
+    public string PlaceName { get; set; }
+    public int Seats { get; set; }
+    public string Status { get; set; } = "empty"; // If you want, we can convert this to TableStatus enum here
+    public string Salt { get; set; } = string.Empty;
+    public bool IsEnabled { get; set; }
+}
---
+++ backend/Bartender.Domain/DTO/UpsertTableDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Bartender.Domain.DTO;
+
+public class UpsertTableDto
+{
+    public int PlaceId { get; set; } //not needed - will pick up manually from UserID?
+
+    public int Seats { get; set; } = 2;
+
+    // Optional: let manager set initial state, or omit entirely and default it in service
+    public string Status { get; set; } = "empty";
+
+    // Optional: allow manager to regenerate salt manually during update
+    public string? Salt { get; set; }
+
+    public bool IsDisabled { get; set; } = false;
+}
