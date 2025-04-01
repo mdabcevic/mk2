7e157dc0053842169fe58ad3a705c8fd38815ebe
maja <maja.dabcevic@gmail.com>
2025-04-01 22:49:36 +0200

    id fetch for frontend

--- backend/Bartender.Domain/DTO/PlaceDto.cs
+++ backend/Bartender.Domain/DTO/PlaceDto.cs
@@ -3,6 +3,7 @@ namespace Bartender.Domain.DTO;
 
 public  class PlaceDto
 {
+    public int Id { get; set; }
     public string? BusinessName { get; set; }
     public required string Address { get; set; }
     public required string CityName { get; set; }
