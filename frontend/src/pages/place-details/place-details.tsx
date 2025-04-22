7a1251ddf2149378cb58de139400f30b62c69cf3
kristijan1808 <kristijan.kerhin@gmail.com>
2025-04-22 08:49:05 +0200

    add:placeholder description on place-details page

--- frontend/src/pages/place-details/place-details.tsx
+++ frontend/src/pages/place-details/place-details.tsx
@@ -38,17 +38,35 @@ const PlaceDetails = () => {
 
         <div className="flex flex-col sm:flex-col sm:justify-between mt-2 mb-3 text-white">
           <p className="">{place?.address}</p>
-          {userRole !== UserRole.guest && (<p className="text-gray-700">{place?.workHours}</p>)}
+          {userRole !== UserRole.guest && (<p className="text-white">{place?.workHours}</p>)}
         </div>
 
         {userRole === UserRole.guest && (<>
           <MyOrders placeId={id!}/>
         </>)}
-
+        {userRole !== UserRole.guest && (
+          <section>
+            
+            <article className="neutral-latte rounded-sm p-5 mb-4">
+              <p className="text-justify">
+              Welcome to {place?.businessName}, where timeless flavors meet modern elegance. Nestled in the heart of {place?.cityName}, we offer a carefully crafted menu of seasonal dishes, premium ingredients, and warm hospitality. Whether you're joining us for a romantic dinner, a family celebration, or a casual lunch, our inviting atmosphere and dedicated team ensure a memorable dining experience.
+              </p>
+              <p className="mt-4">At {place?.businessName}, we believe in honest food made from the freshest, locally sourced ingredients. Our farm-to-table menu changes with the seasons, featuring nourishing dishes inspired by nature. Come enjoy vibrant flavors, thoughtful preparation, and a welcoming space that feels like home.</p>
+            </article>
+            <button className={` px-5 py-1 rounded-[40px] border-mocha ml-1 mr-1 mt-3 bg-mocha-600 text-white`}>
+              <Link to={AppPaths.public.menu.replace(":placeId",id!.toString()!)}>
+              {t("menu").toUpperCase()}
+              </Link>
+            </button>
+          </section>
+          
+        )}
       </div>
 
       {userRole !== UserRole.guest && (place?.images?.length ?? 0) > 0 && (
-          <ImageSlider images={place?.images!} />
+        <div className="px-4">
+          <ImageSlider images={place?.images!}/>
+          </div>
       )}
 
     </div>
