2ba02e1a9580b17ac16592fad4e492661f3fb400
kristina <kristina.anicic1@gmail.com>
2025-04-29 04:13:16 +0200

    add dto

---
+++ backend/Bartender.Domain/DTO/PlaceImage/ImageDto.cs
@@ -0,0 +1,18 @@
+﻿using Bartender.Data.Enums;
+using System.Text.Json.Serialization;
+
+namespace Bartender.Domain.DTO.Picture;
+
+public class ImageDto
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? Id {  get; set; }
+
+    public required string Url { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public ImageType? ImageType { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? IsVisible { get; set; }
+}
