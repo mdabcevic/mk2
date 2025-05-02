0b04d04387ee44cf1144173fdb6523d7ee2c474c
maja <maja.dabcevic@gmail.com>
2025-05-03 01:27:51 +0200

    nullability and cleanup

--- backend/Bartender.Domain/DTO/Staff/LoginStaffDto.cs
+++ backend/Bartender.Domain/DTO/Staff/LoginStaffDto.cs
@@ -3,6 +3,6 @@ namespace Bartender.Domain.DTO.Staff;
 
 public class LoginStaffDto
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public required string Username { get; set; }
+    public required string Password { get; set; }
 }
