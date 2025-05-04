7cb12c6bd787ed58846e17a0865d8d5e786f11cc
kristijan1808 <kristijan.kerhin@gmail.com>
2025-05-04 22:23:16 +0200

    authentication options

--- backend/BartenderBackend/Program.cs
+++ backend/BartenderBackend/Program.cs
@@ -82,7 +82,22 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = new SymmetricSecurityKey(
                 Encoding.UTF8.GetBytes(jwtSettings.Key)),
         };
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
 
+                var path = context.HttpContext.Request.Path;
+                if (!string.IsNullOrEmpty(accessToken) &&
+                    (path.StartsWithSegments("/hubs/place")))
+                {
+                    context.Token = accessToken;
+                }
+
+                return Task.CompletedTask;
+            }
+        };
     });
 
 builder.Services.AddDbContext<AppDbContext>(options =>
