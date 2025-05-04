3525fb5ef2490f6f141b68e6596c32301e43db92
kristina <kristina.anicic1@gmail.com>
2025-05-04 04:40:43 +0200

    add placeId to exception

--- backend/Bartender.Domain/utility/Exceptions/AuthorizationException/UnauthorizedPlaceAccessException.cs
+++ backend/Bartender.Domain/utility/Exceptions/AuthorizationException/UnauthorizedPlaceAccessException.cs
@@ -3,15 +3,21 @@ namespace Bartender.Domain.utility.Exceptions;
 
 public class UnauthorizedPlaceAccessException : AuthorizationException
 {
-
+    public int? PlaceId;
     public UnauthorizedPlaceAccessException()
         : base($"Failure fetching place with requested id.")
     {
     }
+    public UnauthorizedPlaceAccessException(int placeId)
+        : base($"Failure fetching place with requested id.")
+    {
+        PlaceId = placeId;
+    }
 
     public override string GetLogMessage()
     {
-        return $"Cross-entity request detected";
-        //return $"Cross-entity request detected. User {username} ({userId}) tried to access business with id {BusinessId}";
+        return PlaceId.HasValue ?
+            $"Unathorized access to place with ID {PlaceId}" :
+            $"Cross-entity request detected";
     }
 }
