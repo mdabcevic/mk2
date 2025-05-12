374d19ac20870753e77ab8644255003351b886e0
maja <maja.dabcevic@gmail.com>
2025-05-12 17:49:27 +0200

    WIP staff integration tests

---
+++ backend/BartenderTests/IntegrationTests/StaffRepositoryIntegrationTests.cs
@@ -0,0 +1,130 @@
+﻿using Bartender.Data.Enums;
+using Bartender.Data.Models;
+using Bartender.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BartenderTests.IntegrationTests;
+
+[TestFixture]
+public class StaffRepositoryIntegrationTests : IntegrationTestBase
+{
+    [Test]
+    public async Task AddAsync_ThenGetByIdAsync_ShouldReturnStaffWithPlaceAndBusiness()
+    {
+        using var scope = Factory.Services.CreateScope();
+        var staffRepo = scope.ServiceProvider.GetRequiredService<IRepository<Staff>>();
+
+        // Arrange 
+        var staff = new Staff
+        {
+            PlaceId = 1,
+            OIB = "12345678901",
+            Username = "testuserintegration",
+            Password = BCrypt.Net.BCrypt.HashPassword("testpassword"),
+            FullName = "Test User",
+            Role = EmployeeRole.regular
+        };
+
+        // Act
+        await staffRepo.AddAsync(staff);
+        var retrieved = await staffRepo.GetByIdAsync(staff.Id, includeNavigations: true);
+
+        // Assert
+        Assert.That(retrieved, Is.Not.Null, "Staff should be retrieved from database");
+        Assert.Multiple(() =>
+        {
+            Assert.That(retrieved!.Username, Is.EqualTo("testuserintegration"), "Username should match");
+            Assert.That(retrieved.Place, Is.Not.Null, "Staff should be linked to a Place");
+        });
+    }
+
+    [Test]
+    public async Task ExistsAsync_ShouldReturnTrue_WhenUsernameExists()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var staffRepo = scope.ServiceProvider.GetRequiredService<IRepository<Staff>>();
+
+        var staff = new Staff
+        {
+            PlaceId = 1,
+            OIB = "98765432100",
+            Username = "existinguser",
+            Password = BCrypt.Net.BCrypt.HashPassword("password"),
+            FullName = "Exists User",
+            Role = EmployeeRole.regular
+        };
+
+        // Act
+        await staffRepo.AddAsync(staff);
+        var exists = await staffRepo.ExistsAsync(s => s.Username == "existinguser");
+
+        // Assert
+        Assert.That(exists, Is.True, "Staff with given username should exist in the database");
+    }
+
+    [Test]
+    public async Task GetAllAsync_ShouldReturnAllStaffInDb()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var staffRepo = scope.ServiceProvider.GetRequiredService<IRepository<Staff>>();
+
+        await staffRepo.AddAsync(new Staff
+        {
+            PlaceId = 1,
+            OIB = "99999999999",
+            Username = "firstuser",
+            Password = "pwd",
+            FullName = "First User",
+            Role = EmployeeRole.regular
+        });
+
+        await staffRepo.AddAsync(new Staff
+        {
+            PlaceId = 1,
+            OIB = "88888888888",
+            Username = "seconduser",
+            Password = "pwd",
+            FullName = "Second User",
+            Role = EmployeeRole.manager
+        });
+
+        // Act
+        var staff = await staffRepo.GetAllAsync();
+
+        // Assert
+        Assert.That(staff, Has.Count.GreaterThanOrEqualTo(2), "Should return at least two staff records");
+    }
+
+    [Test]
+    public async Task DeleteAsync_ShouldRemoveStaff()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var staffRepo = scope.ServiceProvider.GetRequiredService<IRepository<Staff>>();
+
+        var staff = new Staff
+        {
+            PlaceId = 1,
+            OIB = "11122233344",
+            Username = "deleteuser",
+            Password = "pwd",
+            FullName = "Delete Me",
+            Role = EmployeeRole.regular
+        };
+
+        await staffRepo.AddAsync(staff);
+
+        var inserted = await staffRepo.GetByIdAsync(staff.Id);
+        Assert.That(inserted, Is.Not.Null, "Staff should be inserted before deletion");
+
+        // Act
+        await staffRepo.DeleteAsync(inserted!);
+        var deleted = await staffRepo.GetByIdAsync(staff.Id);
+
+        // Assert
+        Assert.That(deleted, Is.Null, "Staff should be deleted");
+    }
+
+}
---
+++ backend/BartenderTests/IntegrationTests/StaffServiceIntegrationTests.cs
@@ -0,0 +1,72 @@
+﻿using Bartender.Data.Enums;
+using Bartender.Data.Models;
+using Bartender.Domain.DTO.Staff;
+using Bartender.Domain.Interfaces;
+using Bartender.Domain.Utility.Exceptions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BartenderTests.IntegrationTests;
+
+[TestFixture]
+public class StaffServiceIntegrationTests : IntegrationTestBase
+{
+    private IStaffService _service = null!;
+    private IRepository<Staff> _staffRepo = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var scope = Factory.Services.CreateScope();
+        _service = scope.ServiceProvider.GetRequiredService<IStaffService>();
+        _staffRepo = scope.ServiceProvider.GetRequiredService<IRepository<Staff>>();
+    }
+
+    [Test]
+    public async Task AddAsync_ShouldCreateNewStaff()
+    {
+        var dto = new UpsertStaffDto
+        {
+            PlaceId = 1, // seeded
+            Username = "serviceuser",
+            Password = "secret",
+            OIB = "55555555555",
+            FirstName = "Service",
+            LastName = "User",
+            Role = EmployeeRole.regular
+        };
+
+        await _service.AddAsync(dto);
+
+        var exists = await _staffRepo.ExistsAsync(s => s.Username == "serviceuser");
+        Assert.That(exists, Is.True, "New staff should exist in the database after service call");
+    }
+
+    [Test]
+    public async Task AddAsync_ShouldFail_WhenUsernameExists()
+    {
+        var existing = new Staff
+        {
+            PlaceId = 1,
+            OIB = "77777777777",
+            Username = "duplicateuser",
+            Password = "pwd",
+            FullName = "Dupe",
+            Role = EmployeeRole.regular
+        };
+        await _staffRepo.AddAsync(existing);
+
+        var dto = new UpsertStaffDto
+        {
+            PlaceId = 1,
+            Username = "duplicateuser", // same
+            Password = "new",
+            OIB = "77777777777",
+            FirstName = "New",
+            LastName = "Try",
+            Role = EmployeeRole.regular
+        };
+
+        var ex = Assert.ThrowsAsync<ConflictException>(() => _service.AddAsync(dto));
+        Assert.That(ex?.Message, Does.Contain("already exists"));
+    }
+}
