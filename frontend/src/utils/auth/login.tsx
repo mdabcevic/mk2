66e6e3d63baffdd389269de7d6a40d2ae25f2af5
maja <maja.dabcevic@gmail.com>
2025-05-12 10:00:46 +0200

    make it more like a card

--- frontend/src/utils/auth/login.tsx
+++ frontend/src/utils/auth/login.tsx
@@ -1,46 +1,43 @@
 import { useTranslation } from "react-i18next";
 import LoginForm from "./login-form";
-import loginIllustration from "../../../public/assets/images/coffee_shop.jpg"
+import loginIllustration from "../../../public/assets/images/coffee_shop.png"
+import CurveSvg from "../../pages/contact-us/curve-svg";
 
 
 const LoginPage = () => {
   useTranslation("public");
 
   return (
-    <div
-      className="min-h-[80vh] flex items-center justify-center px-4"
-      style={{
-        background: "",
-      }}
-    >
-      {/* main container */}
-      <div className="min-h-screen flex bg-[#F5F0EA]">
-      {/* Left: Illustration */}
-      <div className="hidden lg:flex w-1/2 items-center justify-center">
-        <div className="bg-white border border-[#d1c3aa] rounded-xl shadow-lg h-[85%] w-[90%] flex items-center justify-center p-4">
+    <div className="min-h-screen flex items-center justify-center bg-[#F5F0EA] px-4 py-8">
+      {/* Main Card */}
+      <div className="flex w-full max-w-6xl min-h-[600px] rounded-2xl shadow-lg overflow-hidden border border-[#d9cbb2] bg-white relative">
+        {/* Left: Illustration */}
+        <div className="relative w-1/2 hidden lg:block">
           <img
             src={loginIllustration}
-            alt="Login visual"
-            className="object-contain max-h-full"
+            alt="Cafe illustration"
+            className="w-full h-full object-cover"
           />
+          {/* Optional SVG overlay */}
+          <div className="absolute top-0 right-0 h-full w-20 z-10">
+            <CurveSvg />
+          </div>
+        </div>
+
+        {/* Right: Form Section */}
+        <div className="w-full lg:w-1/2 bg-[#E9DDC9] px-10 py-12 flex items-center justify-center">
+          <div className="w-full max-w-md">
+            <LoginForm />
+          </div>
         </div>
       </div>
 
-      {/* Right: Login Card */}
-      <div className="w-full lg:w-1/2 flex items-center justify-center px-6">
-        <div className="bg-[#E9DDC9] border border-[#b6a584] rounded-2xl shadow-lg p-8 w-full max-w-md">
-          <LoginForm />
-        </div>
-      </div>
-    </div>
-
-
-        {/* Not implemented yet. It will be added in the future! */}
+      {/* Not implemented yet. It will be added in the future! */}
         {/* <p className="text-sm text-center text-gray-600 mt-4">
           Don’t have an account?{" "}
           <span className="text-[#f49241] hover:underline cursor-pointer">Sign up</span>
         </p> */}
-      </div>
+    </div>    
   );
 };
 
