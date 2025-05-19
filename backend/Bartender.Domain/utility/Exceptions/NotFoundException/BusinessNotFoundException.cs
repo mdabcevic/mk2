9ea7e1de87da466cd45058866df9d0c1a5e53593
kristina <kristina.anicic1@gmail.com>
2025-05-19 04:03:36 +0200

    make businessId parameter optional

--- backend/Bartender.Domain/utility/Exceptions/NotFoundException/BusinessNotFoundException.cs
+++ backend/Bartender.Domain/utility/Exceptions/NotFoundException/BusinessNotFoundException.cs
@@ -4,16 +4,19 @@ namespace Bartender.Domain.Utility.Exceptions;
 
 public class BusinessNotFoundException : NotFoundException
 {
-    public int BusinessId { get; }
+    public int? BusinessId { get; }
 
-    public BusinessNotFoundException(int businessId)
+    public BusinessNotFoundException(int? businessId = null)
         : base($"Business was not found.")
     {
-        BusinessId = businessId;
+        if ( businessId != null )
+            BusinessId = businessId;
     }
 
     public override string GetLogMessage()
     {
-        return $"Business with ID {BusinessId} was not found.";
+        return BusinessId.HasValue ? 
+            $"Business with ID {BusinessId} was not found." : 
+            "Business not found";
     }
 }
