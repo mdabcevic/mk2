21b7db72340cda156e6706711c2b9eba4a5da0d7
kristijan1808 <kristijan.kerhin@gmail.com>
2025-05-31 07:33:39 +0200

    change the subscription page style

--- frontend/src/pages/subscription/subscription.tsx
+++ frontend/src/pages/subscription/subscription.tsx
@@ -4,6 +4,7 @@ import SubscriptionTableBody from './subscription-table-body';
 import { subscriptionPlans, benefitsData } from './subscription-data';
 import { useState, useEffect } from "react";
 import ContactModal from "./contact-modal";
+import Footer from "../../containers/footer";
 
 function Subscription() {
     const { t } = useTranslation("public");
@@ -31,11 +32,12 @@ function Subscription() {
     };
 
     return (
-        <div className="flex flex-col max-w-[1200px] mx-auto pt-10 pb-50 px-4">
-            <h2 className="text-3xl text-center font-semibold text-white mt-5">
+        <>
+        <div className="flex flex-col max-w-[1200px] mx-auto pt-50 pb-50 px-4">
+            <h2 className="text-3xl text-center font-semibold text-black mt-5">
                 {t("choose_subscription")}
             </h2>
-            <div className="flex flex-col sm:flex-row items-stretch gap-2 lg:gap-4 mt-15 lg:px-10">
+            <div className="flex flex-col sm:flex-row items-stretch gap-2 lg:gap-4 mt-15 ">
                 {subscriptionPlans.map((subscription, index) => (
                     <SubscriptionCard
                         key={index}
@@ -52,14 +54,14 @@ function Subscription() {
             </div>
 
             <div className="w-full mx-auto mt-20">
-                <div className="overflow-x-auto rounded-tl-3xl rounded-tr-3xl">
-                    <table className="table-auto w-full text-left bg-white rounded-tl-3xl rounded-tr-3xl text-xs sm:text-sm md:text-lg">
+                <div className="overflow-x-auto rounded-2xl border-[#A3A3A3] border-[2px]">
+                    <table className="table-auto w-full text-left bg-white rounded-3xl text-xs sm:text-sm md:text-lg">
                         <thead>
                         <tr className="border border-neutral-300">
                             <th className="px-4 py-2 rounded-tl-2xl  w-1/3">{t("benefits")}</th>
-                            <th className="px-4 py-2 text-center bg-gray-200 w-2/9">Basic {t("subscription")}</th>
-                            <th className="px-4 py-2 text-center bg-blue-200 w-2/9">Pro {t("subscription")}</th>
-                            <th className="px-4 py-2 text-center bg-orange-200 rounded-tr-2xl w-2/9">Premium {t("subscription")}</th>
+                            <th className="px-4 py-2 text-center bg-neutral-300 w-2/9">Basic {t("subscription")}</th>
+                            <th className="px-4 py-2 text-center bg-[#AE8768] w-2/9">Pro {t("subscription")}</th>
+                            <th className="px-4 py-2 text-center bg-brown-500 text-white rounded-tr-2xl w-2/9">Premium {t("subscription")}</th>
                         </tr>
                         </thead>
                         <SubscriptionTableBody t={t} benefits={benefitsData} /> 
@@ -73,7 +75,10 @@ function Subscription() {
                 message={message}
                 t = {t}
             />
+
         </div>
+        <Footer />
+        </>
     );
 }
 export default Subscription;
