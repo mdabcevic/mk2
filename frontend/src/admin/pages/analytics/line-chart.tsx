a2094b3926bd2fdf95b6050ddc3ff41e4e860f8b
kristina <kristina.anicic1@gmail.com>
2025-06-03 20:57:19 +0200

    add line chart component for daily count and earnings

---
+++ frontend/src/admin/pages/analytics/line-chart.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { ResponsiveLine } from '@nivo/line';
+import { TrafficByDayOfWeek } from "./analytics-interface";
+
+interface DailyTraffic {
+    data: TrafficByDayOfWeek[];
+    count: boolean;
+}
+
+const LineChart: React.FC<DailyTraffic> = ({ data,  count}) => {
+    const formattedData = [
+        {
+            id: count ? "Count" : "Earnings",
+            data: data.map(d => ({
+                x: d.dayOfWeek,
+                y: count ? d.count : d.earnings
+            }))
+        }
+    ];
+
+    return (
+    <ResponsiveLine
+        data={formattedData}
+        margin={{ top: 50, right: 110, bottom: 70, left: 80 }}
+        yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+        axisBottom={{ legend: 'dayOfWeek', legendOffset: 50 }}
+        axisLeft={{ legend: count ? "Count" : "Earnings", legendOffset: -60 }}
+        colors={{ scheme: 'brown_blueGreen' }}
+        lineWidth={3}
+        pointColor={{ theme: 'background' }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: 'seriesColor' }}
+        pointLabelYOffset={-12}
+        enableArea={false}
+        areaOpacity={0.15}
+        enableTouchCrosshair={true}
+        useMesh={true}
+    />
+    )
+}
+
+export default LineChart
