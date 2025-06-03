af429c42385cb99f76b2307df9ac7dd867f5dd80
kristina <kristina.anicic1@gmail.com>
2025-06-03 20:35:23 +0200

    add coordinates to place and city

---
+++ backend/Bartender.Data/Migrations/20250602151128_PlaceAndCityCoordinates.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bartender.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlaceAndCityCoordinates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "latitude",
+                table: "places",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "longitude",
+                table: "places",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "latitude",
+                table: "city",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "longitude",
+                table: "city",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "latitude",
+                table: "places");
+
+            migrationBuilder.DropColumn(
+                name: "longitude",
+                table: "places");
+
+            migrationBuilder.DropColumn(
+                name: "latitude",
+                table: "city");
+
+            migrationBuilder.DropColumn(
+                name: "longitude",
+                table: "city");
+        }
+    }
+}
--- backend/Bartender.Data/Models/City.cs
+++ backend/Bartender.Data/Models/City.cs
@@ -6,6 +6,7 @@ public class City : BaseEntity
 {
     [Required]
     public required string Name { get; set; }
-
+    public decimal Longitude { get; set; }
+    public decimal Latitude { get; set; }
     public ICollection<Place> Places { get; set; } = [];
 }
